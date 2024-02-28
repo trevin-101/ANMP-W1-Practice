4fab028fe02585a4239b55282c522d155682bc51
trevin <trevin.juanli@gmail.com>
2024-02-28 20:45:47 +0700

    created login page

---
+++ app/src/main/java/com/nmp/anmp_w1/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.nmp.anmp_w1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    //add code for login below
+}
